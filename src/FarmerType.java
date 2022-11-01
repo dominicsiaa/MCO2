d2817ee68e32cdb5c4c056607fe9423de442480b
nicoledaphneong <nicole_daphne_ong@dlsu.edu.ph>
2022-11-01 16:57:36 +0800

    Farmer Type class instances level requirement and registration fee

--- src/FarmerType.java
+++ src/FarmerType.java
@@ -1,16 +1,20 @@
 public class FarmerType {
     private String type;
+    private int levelRequirement;
     private int bonusEarningsPerProduce;
     private int seedCostReduction;
     private int waterBonusLimitIncrease;
     private int fertilizerBonusLimitIncrease;
+    private int registrationFee;
 
-    public FarmerType(String type, int bonusEarningsPerProduce, int seedCostReduction, int waterBonusLimitIncrease, int fertilizerBonusLimitIncrease) {
+    public FarmerType(String type, int levelRequirement, int bonusEarningsPerProduce, int seedCostReduction, int waterBonusLimitIncrease, int fertilizerBonusLimitIncrease, int registrationFee) {
         this.type = type;
+        this.levelRequirement = levelRequirement;
         this.bonusEarningsPerProduce = bonusEarningsPerProduce;
         this.seedCostReduction = seedCostReduction;
         this.waterBonusLimitIncrease = waterBonusLimitIncrease;
         this.fertilizerBonusLimitIncrease = fertilizerBonusLimitIncrease;
+        this.registrationFee = registrationFee;
     }
 
     public String getType() {
